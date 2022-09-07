4106d4185bb1169c38ea891083da8d9eae7e9d50
Connie Park <conniedc1206@gmail.com>
2022-09-07 11:35:11 -0700

    Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :meow_mails, only: [:index, :create, :destroy]
-    resources :catpanions, only: [:index, :create, :destroy]
-    resources :comments, only: [:index, :create, :destroy]
-    resources :meow_posts
-    resources :users
-    
-    # custom routes
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
-    get "/me", to: "sessions#show"
-  end
+  resources :meow_mails, only: [:index, :create, :destroy]
+  resources :catpanions, only: [:index, :create, :destroy]
+  resources :comments, only: [:index, :create, :destroy]
+  resources :meow_posts
+  resources :users
+  
+  # custom routes
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "sessions#show"
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
